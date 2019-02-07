a7ef8e38426783649997ecd4c97a2fd33e34a012
Trent Davis <trentdavisinc@gmail.com>
2019-02-06 22:57:13 -0600

    Added Add User page

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,12 +1,28 @@
 // Modal functionality
 $(document).ready(function(){
+    // testing only
+    function showAddUsers() {
+        $("#adminHome").hide();
+        $("#addNewGroup").show();
+        $("#loremSelectedUsers").hide();
+    }
     function initAdmin(){
         $("#adminHome").show();
         $("#addNewGroup").hide();
     }
-    initAdmin();
+    showAddUsers();
     $("#submitNewGroupName").on("click", function(){
         $("#adminHome").hide();
         $("#addNewGroup").show();
+    });
+    // test onclick function
+    $("#selectAdd").on("click", function(){
+        $("#loremAvailUsers").hide();
+        $("#loremSelectedUsers").show();
+    });
+    // test onclick function
+    $("#selectRemove").on("click", function(){
+        $("#loremSelectedUsers").hide();
+        $("#loremAvailUsers").show();
     });
 });
