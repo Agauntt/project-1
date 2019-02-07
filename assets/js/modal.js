60f244ff26f2f29d072f95b96f55741a26a53e10
Trent Davis <trentdavisinc@gmail.com>
2019-02-06 23:33:15 -0600

    Added Show Group Modal

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -3,14 +3,18 @@ $(document).ready(function(){
     // testing only
     function showAddUsers() {
         $("#adminHome").hide();
-        $("#addNewGroup").show();
+        $("#addNewGroup").show();       
         $("#loremSelectedUsers").hide();
+        $(".group").hide();
+        
     }
     function initAdmin(){
         $("#adminHome").show();
         $("#addNewGroup").hide();
+        $("#loremSelectedUsers").hide();
+        $(".group").hide();
     }
-    showAddUsers();
+    initAdmin();
     $("#submitNewGroupName").on("click", function(){
         $("#adminHome").hide();
         $("#addNewGroup").show();
@@ -25,4 +29,15 @@ $(document).ready(function(){
         $("#loremSelectedUsers").hide();
         $("#loremAvailUsers").show();
     });
+    // test show groups function
+    $("#addUsersSubmit").on("click", function(){
+        $(".group").show();
+        $("#addNewGroup").hide();
+        $("#adminHome").show();
+    });
+    // test show group modal
+    $("#group1").on("click", function(){
+        $('#showGroupModal').modal('show');  
+    });
+      
 });
