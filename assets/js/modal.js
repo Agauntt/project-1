1885ee3c643d4ca4a0834aaf7e02f071f4d71c3d
Trent Davis <trentdavisinc@gmail.com>
2019-02-07 20:01:49 -0600

    Finish Admin HTML Wire Frame

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -2,22 +2,23 @@
 $(document).ready(function(){
     // testing only
     function showAddUsers() {
-        $("#adminHome").hide();
-        $("#addNewGroup").show();       
+        $("#adminHome").show();
+        $("#addNewGroupUsers").show();       
         $("#loremSelectedUsers").hide();
-        $(".group").hide();
+       
         
     }
     function initAdmin(){
         $("#adminHome").show();
-        $("#addNewGroup").hide();
+        $("#addNewGroupUsers").hide();
         $("#loremSelectedUsers").hide();
+        $("#addActivity").hide();
         $(".group").hide();
     }
     initAdmin();
     $("#submitNewGroupName").on("click", function(){
         $("#adminHome").hide();
-        $("#addNewGroup").show();
+        $("#addNewGroupUsers").show();
     });
     // test onclick function
     $("#selectAdd").on("click", function(){
@@ -32,12 +33,27 @@ $(document).ready(function(){
     // test show groups function
     $("#addUsersSubmit").on("click", function(){
         $(".group").show();
-        $("#addNewGroup").hide();
+        $("#addNewGroupUsers").hide();
         $("#adminHome").show();
     });
     // test show group modal
     $("#group1").on("click", function(){
         $('#showGroupModal').modal('show');  
     });
-      
+    // test add activity modal
+    $("#addNewActivity").on("click", function(){
+        $("#adminHome").hide();
+        $("#addNewGroupUsers").hide();
+        $("#addActivity").show();
+    });
+    // test add single activity
+    $("#addThisActivity").on("click", function(){
+        $('#confirmAddActivityModal').modal('show');  
+    });
+    //test close confirm activity modal
+    $("#confirmAddActivity").on("click", function(){
+        $("#adminHome").show();
+        $("#addNewGroupUsers").hide();
+        $("#addActivity").hide();    
+    });
 });
