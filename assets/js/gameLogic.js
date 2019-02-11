ed0cb8f3c61c9a72026fa3f7afaf77eef982cba5
Aaron.Gauntt <gauntta@gmail.com>
2019-02-11 08:24:49 -0600

    morning commit

--- assets/js/gameLogic.js
+++ assets/js/gameLogic.js
@@ -1,8 +1,23 @@
-var selection1 = "";
+var selection1;
 var selection2 = "";
+var selection3 = "";
+var lie = "";
 
-$(".dropdown").change(function(){
+$("#option1").change(function(){
+    selection1 = this.value;
     console.log(this.value);
 })
 
+$("#option2").change(function(){
+    selection2 = this.value;
+})
 
+$("#option3").change(function(){
+    selection3 = this.value;
+})
+
+$("#input-submit").click(function(){
+    console.log(selection1);
+    console.log(selection2);
+    console.log(selection3);
+})
