7103950d32beaf91967f17e500b11ea82389beb3
Aaron.Gauntt <gauntta@gmail.com>
2019-02-11 09:03:31 -0600

    select screen tied to index.html

--- assets/js/gameLogic.js
+++ assets/js/gameLogic.js
@@ -1,19 +1,21 @@
-var selection1;
-var selection2 = "";
-var selection3 = "";
-var lie = "";
+var selection1 = "1-1";
+var selection2 = "2-1";
+var selection3 = "3-1";
+var lie;
 
-$("#option1").change(function(){
+$("#option1").click(function(){
     selection1 = this.value;
-    console.log(this.value);
+    // console.log(this.value);
 })
 
 $("#option2").change(function(){
     selection2 = this.value;
+    // console.log(this.value);
 })
 
 $("#option3").change(function(){
     selection3 = this.value;
+    // console.log(this.value);
 })
 
 $("#input-submit").click(function(){
