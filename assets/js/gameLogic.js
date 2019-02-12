10a20c90a35cd0cb95eb43bf234dfd59b222147b
Aaron.Gauntt <gauntta@gmail.com>
2019-02-11 18:25:40 -0600

    arrays created and tied in, console.log recognizes selected input

--- assets/js/gameLogic.js
+++ assets/js/gameLogic.js
@@ -1,9 +1,77 @@
-var selection1 = "1-1";
-var selection2 = "2-1";
-var selection3 = "3-1";
+var selection1 = "";
+var selection2 = "";
+var selection3 = "";
 var lie;
 
-$("#option1").click(function(){
+var optionsarr1 = [
+    "I'm a vegetarian",
+    "I'm a vegan",
+    "I've never been outside the US",
+    "I like to go camping",
+    "I enjoy anime",
+    "I prefer movies over TV",
+    "I like to play video games",
+    "My favorite color is blue",
+    "I like to build computers",
+    "I enjoy running"
+];
+
+var optionsarr2 = [
+    "I've been skydiving",
+    "I live on my own",
+    "I know how to drive a manual transmission",
+    "I enjoy lifting weights",
+    "I was born outside the US",
+    "I like to go hunting",
+    "I play World of Warcraft",
+    "I like to go to bars",
+    "I've never had an alcoholic drink",
+    "I enjoy hiking"
+];
+
+var optionsarr3 = [
+    "I enjoy Javascript more than HTML",
+    "I'm more interested in front-end design than backend",
+    "I prefer writing notes over typing them",
+    "I can do my own vehicle maintanence",
+    "I have a fear of water",
+    "I wear glasses/contacts",
+    "I prefer Marvel over DC",
+    "I dislike horror movies",
+    "I know how to ride a motorcycle"
+];
+
+
+function populateDropdown(){
+    for (i = 0; i < optionsarr1.length; i++) {
+        var a = $("<option value=" + "1-" + i + ">" + optionsarr1[i] + "</option>");
+        $("#option1").append(a);
+
+    //     if (selection1 == ""){
+    //         selection1 = "1-0";
+    //     }
+     }
+    for (i = 0; i < optionsarr1.length; i++) {
+        var b = $("<option value=" + "2-" + i + ">" + optionsarr2[i] + "</option>");
+        $("#option2").append(b);
+
+        // if (selection2 == ""){
+        //     selection2 = "2-0";
+        // }
+    }
+    for (i = 0; i < optionsarr1.length; i++) {
+        var c = $("<option value=" + "3-" + i + ">" + optionsarr3[i] + "</option>");
+        $("#option3").append(c);
+
+        // if (selection3 == ""){
+        //     selection3 = "3-0";
+        // }
+    }
+};
+
+populateDropdown();
+
+$("#option1").change(function(){
     selection1 = this.value;
     // console.log(this.value);
 })
@@ -18,8 +86,20 @@ $("#option3").change(function(){
     // console.log(this.value);
 })
 
-$("#input-submit").click(function(){
+
+$("#input-submit").click(function(){ 
+    if (selection1 == ""){
+        selection1 = "1-0";
+    }
+
+    if (selection2 == ""){
+        selection2 = "2-0";
+    }
+
+    if (selection3 == ""){
+        selection3 = "3-0";
+    }
     console.log(selection1);
     console.log(selection2);
     console.log(selection3);
-})
+});
