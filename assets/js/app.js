5b2dd18095a3014481391d453fb8a0bacd5976bd
Trent Davis <trendavisinc@gmail.com>
2019-02-13 00:00:17 -0600

    Added status badge CSS

--- assets/js/app.js
+++ assets/js/app.js
@@ -140,8 +140,10 @@ $(document).ready(function () {
                     html += "<li>" + qtyComp + " Complete</li>";
                 }
                 $("#adminShowResults").html(html);
+                $("#showGroupResults").removeClass("disabled");
             } else {
                 $("#adminShowResults").html("<li>You do not have any results to display</li>");
+                $("#showGroupResults").addClass("disabled");
             }
 
         });
@@ -258,21 +260,33 @@ $(document).ready(function () {
     }
     // print results page to HTML
     function printResultsToHTML(groupID){
+        $(".result-nav-link").empty();
+        $(".result-nav-link").removeClass("active");
+        $("#resultTabContent").empty();
+        $(".tab-pane").removeClass("show active"); 
         var i = 0;
-        db.ref('results').orderByChild('group_id').equalTo(groupID).on("value", function (snap) {           
+        db.ref('results').orderByChild('group_id').equalTo(groupID).once("value", function (snap) {           
             snap.forEach(function (child) {      
                 var cv = child.val();             
                 var newLi = "<li class='nav-item '>";
                 newLi += "<a class='nav-link result-nav-link' id='" + cv.activity_key + "-tab' data-toggle='tab' href='#" + cv.activity_key + "'";                
-                newLi += "role='tab'>Result" + cv.activity_id + "</a></li>";
+                newLi += "role='tab'></a></li>";
                 $("#resultTab").append(newLi);
                 i++;
                 var html = "<div class='tab-pane fade  result-tab-content' id='" + cv.activity_key + "'";                              
-                html += "role='tabpanel'><div class='row'><div class='col-6'><h3 id='title-" + cv.activity_key + "'></h3>";
-                html += "<small id='desc-" + cv.activity_key + "'></small></div><div class='col-6'>"; 
+                html += "role='tabpanel'><div class='row'><div class='col-6'><h3 class='results-activity-title' id='title-" + cv.activity_key + "'></h3>";
+                html += "<span id='status-" + cv.activity_key + "' class='badge results-activity-status'>" + cv.status + "</span>";
+                html += "<small class='results-activity-desc' id='desc-" + cv.activity_key + "'></small></div><div class='col-6'>"; 
                 html += "<h3>Suggested Venues</h3><img src='assets/images/resultDemo.jpg' width='500' />";        
                 html += "</div></div></div>";      
                 $("#resultTabContent").append(html);  
+                if(cv.status == "pending"){
+                    $("#status-" + cv.activity_key).addClass("badge-warning");                  
+                } else if(cv.status == "in progress"){
+                    $("#status-" + cv.activity_key).addClass("badge-info");                  
+                } else if(cv.status == "complete"){
+                    $("#status-" + cv.activity_key).addClass("badge-success");                  
+                }
                 db.ref('activities').orderByChild('activity_id').equalTo(cv.activity_id).on("child_added", function (activitychild) {
                     $("#" + cv.activity_key + "-tab").text(activitychild.key);
                     $("#title-" + cv.activity_key).text(activitychild.key);
